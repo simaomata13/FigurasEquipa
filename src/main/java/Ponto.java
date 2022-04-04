19e3db2c0b9f63378f2269c572edba4476ac9c76
Carlos Fernandes <carlos_10_porto@hotmail.com>
2022-04-04 16:57:34 +0100

    Nova classe Ponto

---
+++ src/main/java/Ponto.java
@@ -0,0 +1,25 @@
+public class Ponto {
+    private float pontoX;
+    private float pontoY;
+
+    public Ponto(float pontoX, float pontoY) {
+        this.pontoX = pontoX;
+        this.pontoY = pontoY;
+    }
+
+    public float getPontoX() {
+        return pontoX;
+    }
+
+    public void setPontoX(float pontoX) {
+        this.pontoX = pontoX;
+    }
+
+    public float getPontoY() {
+        return pontoY;
+    }
+
+    public void setPontoY(float pontoY) {
+        this.pontoY = pontoY;
+    }
+}
