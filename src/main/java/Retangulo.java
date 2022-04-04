68b2f8d0afc02f66d56b48e68b8584ca15a38779
Carlos Fernandes <carlos_10_porto@hotmail.com>
2022-04-04 17:16:22 +0100

    Atributo Cores

--- src/main/java/Retangulo.java
+++ src/main/java/Retangulo.java
@@ -1,2 +1,11 @@
 public class Retangulo {
+    private String cor;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 }
