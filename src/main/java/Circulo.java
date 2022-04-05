de017fcdc9cd5b9842b611aafb19de97b5461f7f
Simao Mata <scfamata@gmail.com>
2022-04-05 09:47:35 +0100

    atualizar

--- src/main/java/Circulo.java
+++ src/main/java/Circulo.java
@@ -1,11 +1,4 @@
 import java.awt.*;
-<<<<<<< HEAD
-
-public class Circulo {
-    private double raio;
-    private Point centro;
-=======
->>>>>>> ca2e3e63e811adeba769ee41b7926dc956c87d64
 
 public class Circulo extends Figure {
     private Point center;
